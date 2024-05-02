d8f1b566d91ac10c0666e1422fbbce60e9b6357c
Abhishek Hugar <157807210+abhihuga6@users.noreply.github.com>
2024-05-02 15:39:46 +0530

    Add files via upload

---
+++ login prog.py
@@ -0,0 +1,6 @@
+# login program
+
+username=input("username")
+password=input("password")
+print("welcome") if username=="nit" and password=="nit123" else print("invalid user name or password")
+          
