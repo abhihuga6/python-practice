527944dd169b307f3a99ec921dabdbd52e37aa7e
Abhishek Hugar <157807210+abhihuga6@users.noreply.github.com>
2024-05-04 17:32:23 +0530

    Add files via upload

---
+++ dollar to rs.py
@@ -0,0 +1,5 @@
+#write a program to convert dollar$ to rs
+amt_dollar=int(input("enter the amount in dollar"))
+amt_rs=amt_dollar*90
+print("amount in dollar",amt_dollar)
+print("amount in rs",amt_rs)
